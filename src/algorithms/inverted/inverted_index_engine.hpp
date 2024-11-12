45ab5730b931adec8aac8236b6ad5a954a636c85
Julian-JP <julian.laux@tum.de>
2024-11-12 23:19:52 +0100

    Base implementation of inverted index

--- src/algorithms/inverted/inverted_index_engine.hpp
+++ src/algorithms/inverted/inverted_index_engine.hpp
@@ -9,11 +9,12 @@
 
 class InvertedIndexEngine : public FullTextSearchEngine {
  public:
-  void indexDocuments(DocumentIterator it) override;
+  void indexDocuments(DocumentIterator doc_it) override;
 
   std::vector<uint32_t> search(const std::string &query) override;
 
  private:
+    std::unordered_map<std::string, std::unordered_map<size_t, size_t>> index;
 };
 
 #endif  // INVERTEDINDEXENGINE_HPP
