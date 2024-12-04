78070a0681ed8d8dd03f03be2e4ef7877a56007b
Nicolas Schmitt <ge93cuy@mytum.de>
2024-12-04 12:35:34 +0100

    Make the document ID unsigned

--- src/documents/document.hpp
+++ src/documents/document.hpp
@@ -1,6 +1,7 @@
 #ifndef DOCUMENT_HPP
 #define DOCUMENT_HPP
 
+#include <cstdint>
 #include <parquet/arrow/reader.h>
 #include <arrow/io/file.h>
 
@@ -9,14 +10,14 @@ class Document {
     Document(int id, const char *data, size_t size, const std::shared_ptr<arrow::Buffer> &arrow_buf)
         : id(id), data(data), size(size), arrow_buf(arrow_buf) {};
 
-    [[nodiscard]] int getId() const { return id; }
+    [[nodiscard]] uint32_t getId() const { return id; }
 
     [[nodiscard]] const char *getData() const { return data; }
 
     [[nodiscard]] size_t getSize() const { return size; }
 
     private:
-    int id;
+    uint32_t id;
     const char *data;
     const size_t size;
     const std::shared_ptr<arrow::Buffer> &arrow_buf;
