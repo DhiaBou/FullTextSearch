c9b0fc92061785a8efda064517ff4b858ca1c5d3
Miguel Marcano <miguel131299@gmail.com>
2024-12-05 20:09:03 +0100

    add basic HNSW structure

---
+++ src/algorithms/VectorSpaceModel/VectorSpaceModelEngine.hpp
@@ -0,0 +1,22 @@
+//
+// Created by fts on 10/31/24.
+//
+
+#ifndef VECTORSPACEMODELENGINE_HPP
+#define VECTORSPACEMODELENGINE_HPP
+
+#include "../../FullTextSearchEngine.hpp"
+
+class VectorSpaceModelEngine : public FullTextSearchEngine {
+public:
+
+    VectorSpaceModelEngine();
+
+    void indexDocuments(DocumentIterator it) override;
+
+    std::vector<std::shared_ptr<Document> > search(const std::string &query) override;
+
+private:
+};
+
+#endif  // VECTORSPACEMODELENGINE_HPP
