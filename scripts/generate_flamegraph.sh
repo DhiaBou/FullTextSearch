65879b509c9bfdb6f9594e9eae4eb04dcd445836
dhia <dhia.bouassida@tum.de>
2025-03-15 14:32:46 +0100

    add flamegraph generating script

--- scripts/generate_flamegraph.sh
+++ scripts/generate_flamegraph.sh
@@ -3,11 +3,9 @@
 # Install required tools if needed
 # sudo apt-get install linux-tools-common linux-tools-generic linux-tools-`uname -r`
 
-# Clone FlameGraph if needed
-
 # Record performance data
 echo "Recording performance data..."
-sudo perf record -F 99 -g -a -- cmake-build-debug/fts -d data/guthenberg/pip -a inverted -s tf-idf
+sudo perf record -F 99 -g -a -- ../cmake-build-debug/fts -d ../data/ppip -a inverted -s tf-idf
 
 
 FLAMEGRAPH_DIR="/home/dhia/Downloads/FlameGraph"
@@ -18,6 +16,8 @@ echo "Generating flamegraph..."
 sudo perf script > out.perf
 $FLAMEGRAPH_DIR/stackcollapse-perf.pl out.perf > out.folded
 $FLAMEGRAPH_DIR/flamegraph.pl out.folded > flamegraph.svg
-
+rm out.perf out.folded
+rm -f perf.data
+rm -f perf.data.old
 echo "Flamegraph generated as flamegraph.svg"
 echo "Open it in your web browser to view"
