175bda1f75103726363fefa175eb53bfc469136f
Nicolas Schmitt <ge93cuy@mytum.de>
2025-03-24 09:11:51 +0100

    Add benchmarking capabilities

---
+++ scripts/bench/benchmark.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+import csv
+import argparse
+from datetime import datetime
+
+# -------------- Setup --------------
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Benchmark script for FullTextSearch.")
+parser.add_argument('-d', '--data', required=True, help="Path to the data directory.")
+parser.add_argument('-e', '--executable', required=True, help="Path to the FullTextSearch executable.")
+parser.add_argument('-o', '--output', required=True, help="Path to the output directory.")
+parser.add_argument('-q', '--query', required=True, help="Path to the query directory.")
+parser.add_argument('-n', '--num_results', required=True, help="The number of results per query.")
+args = parser.parse_args()
+
+data_src = args.data
+executable_path = args.executable
+output_dir = args.output
+query_dir = args.query
+num_results = args.num_results
+data_sets = ['amazon_review']
+algos = ['trigram']
+
+# Ensure directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+# -------------- Benchmarks --------------
+
+# BUILD
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+build_file = f'{output_dir}/build_{timestamp}.csv'
+footprint_file = f'{output_dir}/footprint_{timestamp}.csv'
+query_file = f'{output_dir}/query_{timestamp}.csv'
+with open(build_file, 'w', newline='') as build,\
+    open(footprint_file, 'w', newline='') as footprint,\
+    open(query_file, 'w', newline='') as query:
+
+    build_writer = csv.writer(build)
+    footprint_writer = csv.writer(footprint)
+    query_writer = csv.writer(query)
+    
+    for data in data_sets:
+        for algo in algos:
+            print(f"{algo} on {data}")
+            result = subprocess.run([
+                executable_path,
+                '-d', os.path.join(data_src, data),
+                '-a', algo,
+                '-s', 'bm25',
+                '-n', num_results,
+                '-q', query_dir
+            ], capture_output=True, text=True)
+
+            for line in result.stdout.splitlines():
+                if line.startswith("Build:"):
+                    build_time = line.split(":")[1].strip()
+                    build_writer.writerow([data, algo, build_time])
+                elif line.startswith("Memory footprint:"):
+                    memory_footprint = line.split(":")[1].strip()
+                    footprint_writer.writerow([data, algo, memory_footprint])
+                else:
+                    print(line)
+                    query_id = line.split(":")[0].strip()  # Remove quotes from the query
+                    time = line.split(":")[1].strip()  # Extract the time
+                    query_writer.writerow([data, algo, query_id, time])
